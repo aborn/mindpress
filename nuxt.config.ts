271bbeedcc1684e73f353167fddc1df3096867a9
Aborn Jiang <guobao.jiang@aliyun.com>
2022-05-18 21:42:43 +0800

    minial start

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,20 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    app: {
+        head: {
+            link: [
+                {
+                    rel: 'stylesheet',
+                    href: 'https://unpkg.com/@picocss/pico@latest/css/pico.min.css'
+                }
+            ]
+        }
+    },
+    modules: [
+        '@nuxt/content'
+    ],
+    content: {
+        // Options
+    }
 })
