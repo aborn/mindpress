aa480c706117277a36049a260648ce33910ad4ea
Aborn Jiang <guobao.jiang@aliyun.com>
2022-05-20 22:49:44 +0800

    style v1

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,19 @@
   <main class="container">
     <div class="articles">
       <div class="article" v-for="article in articles" :key="article.id">
-          <article>
-            <header>{{ article.title }}</header>
-            <a :href="`${article.path}`">
-            {{ article.description }}
-            </a>
-            <footer>{{ article.mtime }}</footer>
-          </article>
+        <div style="flex-shrink: 0">
+          <img src="/assets/img/flex.JPG" alt="flex demo" loading="lazy" width="480px" height="auto"
+            sizes="sm:355px md:320px lg:480px" class="card-image" />
+        </div>
+        <div class="card-content">
+          <a :href="`${article.path}`">
+            <h2 class="text-gray-900">{{ article.title }}</h2>
+            <p class="text-gray-500">{{ article.description }}</p>
+          </a>
+          <div class="card-footer">
+            <div class="card-footer-date">May 19, 2022</div>
+          </div>
+        </div>
       </div>
     </div>
   </main>
