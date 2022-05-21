7d0046b34c7349afa72d3a4ee2a6ce9bc6e9f702
Aborn Jiang <guobao.jiang@aliyun.com>
2022-05-21 22:02:27 +0800

    word dealing

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -6,7 +6,7 @@
         <div class="card-content">
             <a :href="`${item.path}`">
                 <h2 class="text-gray-900">{{ item.title }}</h2>
-                <p class="text-gray-500">{{ item.description }}</p>
+                <p class="text-gray-500">{{ formatDesc(item.description) }}</p>
             </a>
             <div class="card-footer">
                 <div class="card-footer-date">
@@ -30,6 +30,13 @@ export default {
         }
     },
     methods: {
+        formatDesc(des) {
+            const maxLength = 50;
+            const cjkMatch = des.match(/[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f]/) 
+            const trimmedString = des.trim().substr(0, maxLength);
+            const desCutWord = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
+            return cjkMatch ?  trimmedString + "..." : desCutWord
+        },
         formatDate(date) {
             const options = { year: 'numeric', month: 'long', day: 'numeric' }
             return new Date(date).toLocaleDateString('en', options)
