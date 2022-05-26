99ed1304d04b0b1933dd69dce4c58bad56363200
Aborn Jiang <guobao.jiang@aliyun.com>
2022-05-26 10:15:56 +0800

    rename

--- composables/parseText.ts
+++ composables/parseText.ts
@@ -1,7 +1,7 @@
-import hl from "./hl"
+import shiki from "./shiki"
 
 export async function parseText(content: any) {
     const parsed = await parseMdContent(DEMO_TEXT_MARKDOWN);
-    const parsedHighlight = await hl.transform(parsed)
+    const parsedHighlight = await shiki.transform(parsed)
     return parsed;
 }
