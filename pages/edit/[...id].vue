df13d8480381c339698adb82c14ed7e09a2c0d0a
Aborn Jiang <guobao.jiang@aliyun.com>
2022-05-30 15:34:12 +0800

    edit & update

--- pages/edit/[...id].vue
+++ pages/edit/[...id].vue
@@ -2,8 +2,8 @@
     <NavBar />
     <main class="container">
         <ColorScheme placeholder="..." tag="span">
-            <md-editor v-model="mkdContent" :theme="$colorMode.value" :toolbarsExclude="toolbarsExclude" style="height:480px;"
-                @onChange="changeAction" @onSave="saveAction" />
+            <md-editor v-model="mkdContent" :theme="$colorMode.value" :toolbarsExclude="toolbarsExclude"
+                style="height:480px;" @onChange="changeAction" @onSave="saveAction" />
         </ColorScheme>
     </main>
 </template>
@@ -17,34 +17,72 @@ import 'md-editor-v3/lib/style.css';
 
 const route = useRoute()
 const text = ref(DEMO_TEXT_MARKDOWN)
-const id = route.params.id
+const articleids = route.params.id
+const articleid = articleids[0]
 const toolbarsExclude = ['github']
-console.log('id=' + id)
 
-const url = 'http://localhost:3012/api/content/' + id
+const url = 'http://localhost:3012/api/content/' + articleid
 console.log(url)
 const { data } = await useFetch(url)
-console.log(data)
+// console.log(data)
 
 const processData = (data) => {
     if (data.value) {
-        return data.value.content
+        return {
+            content: data.value.content,
+            id: data.value.id,
+            msg: 'success',
+            status: true
+        }
     } else {
         console.log('error in http')
-        return ''
+        return {
+            content: "",
+            id: 0,
+            msg: "error http fetch content, articleid=" + articleid,
+            status: false
+        }
     }
 }
 
-const mkdContent = ref(processData(data))
-//console.log(mkdContent)
+const pData = processData(data);
+const mkdContent = ref(pData.content)
+const id = pData.id
+// console.log(mkdContent)
 
 const changeAction = (e) => {
     console.log('content changed. data=' + new Date())
 }
 
 const saveAction = (text) => {
-    console.log('--- now save event triggled. ---')
-    console.log(text)
+    console.log('--- now save event triggled. articleid=' + articleid + '---')
+    // console.log(text)
+    const cuUrl = 'http://localhost:3012/api/content'
+    // console.log('id=' + id + ', articleid=' + articleid)
+    useFetch(cuUrl,
+        {
+            key: "cu_action",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: {
+                "articleid": articleid,
+                "content": text
+            }
+        }).then(res => {
+            const data = res.data.value
+            const error = res.error.value
+            if (error) {
+                // dealing error
+                console.log(error)
+            } else {
+                console.log(data)
+            }
+        }, error => {
+            console.log('exception...')
+            console.log(error)
+        })
 }
 
 </script>
