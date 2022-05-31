44738ebf91d529582f8862155bde9bb9b17eee57
Aborn Jiang <guobao.jiang@aliyun.com>
2022-05-31 10:37:55 +0800

    show remote card list

--- pages/index.vue
+++ pages/index.vue
@@ -9,13 +9,28 @@
   </main>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { ref } from "vue"
 const articles = ref([]);
+const url = 'http://localhost:3012/api/markdownMeta'
 
 async function query() {
   const { data } = await useAsyncData('home', () => queryContent().find())
-  articles.value = data.value;
+  console.log(data.value)
+  const { data: dataServer } = await useFetch(url)
+
+  console.log(dataServer.value)
+
+  if (dataServer.value.totalElements > 0) {
+    const dataS = ref([])
+    dataS.value = dataServer.value.content.map((value) => {
+      return mpTransform(value)
+    })
+    console.log(dataS)
+    articles.value = dataS.value;
+  } else {
+    articles.value = data.value;
+  }
 }
 
 query();
