b12309288a1954b6dfa7050c58da971ae8e595be
Aborn Jiang <guobao.jiang@aliyun.com>
2022-06-01 11:13:50 +0800

    add hint for tag search

--- pages/tag/[...tag].vue
+++ pages/tag/[...tag].vue
@@ -5,21 +5,25 @@ const mp = mpConfig(useRuntimeConfig().public.minpress)
 const route = useRoute()
 const tags = route.params.tag
 const tag = tags[0]
+const articles = ref([]);
 
-const url = this.mp.searchUrl + "?tag=" + tag
+const url = mp.searchUrl + "?tag=" + tag
 console.log(url)
 
 const { data } = await useFetch(url)
 // console.log(data.value)
+
 if (data.value.totalElements > 0) {
     const dataS = ref([])
     dataS.value = data.value.content.map((value) => {
         return mpTransform(value)
     })
     // console.log(dataS)
-    const articles = dataS.value;
+    articles.value = dataS.value;
+    const hint = ''    
 } else {
-    const articles = []
+    articles.value = []
+    const hint = 'not find.'
 }
 
 </script>
@@ -27,6 +31,7 @@ if (data.value.totalElements > 0) {
 <template>
     <NavBar />
     <main class="container">
+        <label>{{ hint }}</label>
         <div class="articles">
             <div class="article" v-for="article in articles" :key="article.id">
                 <PostCard :item="article" />
