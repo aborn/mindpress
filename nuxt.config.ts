a9b45a140a717562ac02c27dd322bf76f9cb87c8
aborn <loveaborn@foxmail.com>
2022-07-08 19:44:58 +0800

    static mindpress

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    target: 'static',
+    router: {
+      base: '/mindpress/'
+    },
     app: {
         head: {
             title: 'MindPress, Press your mind gracefully.',
