f5d17ca8ce3ca51e46726ac3262b4af671a9b274
aborn <loveaborn@foxmail.com>
2022-07-09 16:31:48 +0800

    single root

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,19 +1,21 @@
 <template>
-  <NavBar />
-  <main class="container">
-    <ContentDoc v-slot="{ doc }">
-      <div class="article-title">{{ doc.title }}</div>
-      <!-- todo: show authors.
+  <div>
+    <NavBar />
+    <main class="container">
+      <ContentDoc v-slot="{ doc }">
+        <div class="article-title">{{ doc.title }}</div>
+        <!-- todo: show authors.
       <div v-for="author of doc" :key="author.name">
         {{ author.name }}
       </div>
       -->
 
-      <ContentRenderer :value="doc">
-        <template #empty>
-          <p>No contents found.</p>
-        </template>
-      </ContentRenderer>
-    </ContentDoc>
-  </main>
+        <ContentRenderer :value="doc">
+          <template #empty>
+            <p>No contents found.</p>
+          </template>
+        </ContentRenderer>
+      </ContentDoc>
+    </main>
+  </div>
 </template>
