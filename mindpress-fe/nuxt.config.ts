8b5be85c8717e612547bc7739b1b8e9069dc6dbe
aborn <loveaborn@foxmail.com>
2024-06-02 09:01:28 +0800

    mode static

--- mindpress-fe/nuxt.config.ts
+++ mindpress-fe/nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
     runtimeConfig: {
         public: {
             minpress: {
-                mode: 'server',   // 'static' (default) or 'server'
+                mode: 'static',   // 'static' (default) or 'server'
                 baseUrl: 'http://localhost:3012/api/mindpress/'
             }
         }
