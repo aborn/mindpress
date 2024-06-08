0f054ee3a0d2cf2a2fc1f7567ae0fe2530143afe
aborn <loveaborn@foxmail.com>
2024-06-08 08:55:05 +0800

    add status.

---
+++ mindpress-fe/server/api/md/status.ts
@@ -0,0 +1,15 @@
+import { defineEventHandler } from 'h3'
+import fs from 'node:fs';
+
+export default defineEventHandler(async (event) => {
+    console.log("nitro: req comming...(status)")
+    const req = event.node.req
+    const query = getQuery(event)
+    let data = '';
+    console.log(req.url)
+    
+    return {
+        md: data,
+        api: 'mindpress works',
+    }
+})
