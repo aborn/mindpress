bbbb734b47ab5492c5962fe7ce6d3771540d23e5
aborn <loveaborn@foxmail.com>
2024-06-19 15:00:25 +0800

    search page update & my docs page update.

--- mindpress-fe/pages/search/index.vue
+++ mindpress-fe/pages/search/index.vue
@@ -4,7 +4,7 @@
     <main class="container">
       <form @submit.prevent="submit" style="display: flex;justify-content: center;margin-bottom:0rem">
         <input type="text" style="height:2.5rem" v-model="search" placeholder="Please input your keyword." />
-        <button class="outline" style="margin-left: .5rem;height:2.5rem;width: 10rem;">Search</button>
+        <UButton icon="i-heroicons-magnifying-glass-16-solid" style="width: 10rem;margin-left: 10px" block>Search</UButton>
       </form>
 
       <label style="margin-bottom:1rem" v-html="hint"></label>
