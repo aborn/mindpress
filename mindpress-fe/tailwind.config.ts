74ab923f9636e655d0092d54da1a4d7fa0c14378
aborn <loveaborn@foxmail.com>
2024-06-19 11:15:43 +0800

    disable preflight in tailwind.css

---
+++ mindpress-fe/tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss'
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      aspectRatio: {
+        auto: 'auto',
+        square: '1 / 1',
+        video: '16 / 9'
+      }
+    }
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [
+    // require('@tailwindcss/typography'),
+    // ...
+  ],
+}
