8097de4f85b4c1265d26bbadb67ede8d244b2dcd
aborn <loveaborn@foxmail.com>
2024-07-13 15:10:41 +0800

    magiceditor commands module.

---
+++ mindpress-fe/unjs/editor/magiceditor/basicSetup.ts
@@ -0,0 +1,104 @@
+import {
+  keymap, highlightSpecialChars, drawSelection, highlightActiveLine, dropCursor,
+  rectangularSelection, crosshairCursor,
+  lineNumbers, highlightActiveLineGutter
+} from "@codemirror/view"
+import { Extension, EditorState } from "@codemirror/state"
+import {
+  defaultHighlightStyle, syntaxHighlighting, indentOnInput, bracketMatching,
+  foldGutter, foldKeymap
+} from "@codemirror/language"
+import {
+  insertTab, indentLess, defaultKeymap, cursorSyntaxLeft, selectSyntaxLeft, selectSyntaxRight, cursorSyntaxRight, moveLineUp, copyLineUp, moveLineDown, copyLineDown, simplifySelection,
+  insertBlankLine, selectLine, selectParentSyntax, indentMore, indentSelection, deleteLine, cursorMatchingBracket, toggleComment, toggleBlockComment, toggleTabFocusMode, standardKeymap, history, historyKeymap
+} from '@codemirror/commands'
+import { searchKeymap, highlightSelectionMatches } from "@codemirror/search"
+import { autocompletion, completionKeymap, closeBrackets, closeBracketsKeymap } from "@codemirror/autocomplete"
+import { lintKeymap } from "@codemirror/lint"
+
+// https://github.com/codemirror/basic-setup/blob/main/src/codemirror.ts
+export const basicSetup: Extension = (() => [
+  lineNumbers(),
+  highlightActiveLineGutter(),
+  highlightSpecialChars(),
+  history(),
+  foldGutter(),
+  // drawSelection(),
+  dropCursor(),
+  EditorState.allowMultipleSelections.of(true),
+  indentOnInput(),
+  syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+  bracketMatching(),
+  closeBrackets(),
+  autocompletion(),
+  rectangularSelection(),
+  crosshairCursor(),
+  highlightActiveLine(),
+  highlightSelectionMatches(),
+  keymap.of([
+    ...closeBracketsKeymap,
+    ...defaultKeymap,
+    ...searchKeymap,
+    ...historyKeymap,
+    ...foldKeymap,
+    ...completionKeymap,
+    ...lintKeymap
+  ])
+])()
+
+/// A minimal set of extensions to create a functional editor. Only
+/// includes [the default keymap](#commands.defaultKeymap), [undo
+/// history](#commands.history), [special character
+/// highlighting](#view.highlightSpecialChars), [custom selection
+/// drawing](#view.drawSelection), and [default highlight
+/// style](#language.defaultHighlightStyle).
+export const minimalSetup: Extension = (() => [
+  highlightSpecialChars(),
+  history(),
+  drawSelection(),
+  syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+  keymap.of([
+    ...defaultKeymap,
+    ...historyKeymap,
+  ])
+])()
+
+const demoCommand = ({ state, dispatch }) => {
+  console.log('demo command click...')
+};
+type KeyBinding = /*unresolved*/ any
+
+// https://github.com/codemirror/commands/blob/main/src/commands.ts
+export const myDefaultKeymap: readonly KeyBinding[] = ([
+  { key: "Alt-ArrowLeft", mac: "Ctrl-ArrowLeft", run: cursorSyntaxLeft, shift: selectSyntaxLeft },
+  { key: "Alt-ArrowRight", mac: "Ctrl-ArrowRight", run: cursorSyntaxRight, shift: selectSyntaxRight },
+
+  { key: "Alt-ArrowUp", run: moveLineUp },
+  { key: "Shift-Alt-ArrowUp", run: copyLineUp },
+
+  { key: "Alt-ArrowDown", run: moveLineDown },
+  { key: "Shift-Alt-ArrowDown", run: copyLineDown },
+
+  { key: "Escape", run: simplifySelection },
+  { key: "Mod-Enter", run: insertBlankLine },
+
+  { key: "Alt-l", mac: "Ctrl-l", run: selectLine },
+  { key: "Alt-,", mac: "Ctrl-,", run: demoCommand },
+  { key: "Mod-i", run: selectParentSyntax, preventDefault: true },
+
+  { key: "Mod-[", run: indentLess },
+  { key: "Mod-]", run: indentMore },
+  { key: "Mod-Alt-\\", run: indentSelection },
+
+  { key: "Shift-Mod-k", run: deleteLine },
+
+  { key: "Shift-Mod-\\", run: cursorMatchingBracket },
+
+  { key: "Mod-/", run: toggleComment },
+  { key: "Alt-A", run: toggleBlockComment },
+
+  { key: "Ctrl-m", mac: "Shift-Alt-m", run: toggleTabFocusMode },
+] as readonly KeyBinding[]).concat(standardKeymap)
+
+
+export { EditorView } from "@codemirror/view"
---
+++ mindpress-fe/unjs/editor/magiceditor/commands.ts
@@ -0,0 +1,94 @@
+import { expectTypeOf } from 'vitest'
+import { EditorView } from './basicSetup'
+import { EditorSelection, SelectionRange, Compartment } from '@codemirror/state'
+import * as prettier from 'prettier/standalone'
+import pluginMarkdown from 'prettier/plugins/markdown'
+
+function sliceDoc(editor: EditorView, from?: number, to?: number): string {
+    return editor.state.sliceDoc(from, to)
+}
+
+function getDocString(editor: EditorView): string {
+    return editor.state.doc.toString()
+}
+
+function getDocLength(editor: EditorView): number {
+    return editor.state.doc.length
+}
+
+function insert(editor: EditorView, istFrom: number, istTo: number, content: string, selectFrom: number, selectTo: number) {
+    let changeByRange = {
+        /* 创建变更的内容, 可以是个数组, 说明同时修改多个部分 */
+        changes: [{ from: istFrom, to: istTo, insert: content }],
+        /* 修改之后光标移动到的位置 */
+        selection: EditorSelection.create([EditorSelection.range(selectFrom, selectTo)])
+    }
+    // editor.dispatch(
+    //   /**
+    //    * @param _range 当前选中的位置
+    //    */
+    //   editor.state.changeByRange((_range: SelectionRange) => {
+    //     console.log(_range);
+    //     return changeByRange
+    //   })
+    // )
+    editor.dispatch(changeByRange)
+}
+
+function replaceInlineCommand(editor: EditorView, range: SelectionRange, target: string): any {
+    let len = target.length
+
+    const prefixFrom: number = range.from - len
+    const prefixTo: number = range.from
+    const prefix = sliceDoc(editor, prefixFrom, prefixTo)
+
+    const suffixFrom: number = range.to
+    const suffixTo: number = range.to + len
+    const suffix = sliceDoc(editor, suffixFrom, suffixTo)
+    // 判断是取消还是添加, 如果被选中的文本前后已经是 target 字符, 则删除前后字符
+    if (prefix == target && suffix == target) {
+        return {
+            changes: [
+                { from: prefixFrom, to: prefixTo, insert: '' },
+                { from: suffixFrom, to: suffixTo, insert: '' }
+            ],
+            range: EditorSelection.range(prefixFrom, suffixFrom - len)
+        }
+    } else {
+        return {
+            changes: [
+                { from: range.from, insert: target },
+                { from: range.to, insert: target }
+            ],
+            range: EditorSelection.range(range.from + len, range.to + len)
+        }
+    }
+}
+
+export function commandBold(_view: EditorView) {
+    _view.dispatch(_view.state.changeByRange((range: SelectionRange) => {
+        return replaceInlineCommand(_view, range, '**');
+    }))
+    return true;
+}
+
+export function commandItalic(editor: EditorView) {
+    editor.dispatch(editor.state.changeByRange((range: SelectionRange) => replaceInlineCommand(editor, range, '*')))
+    return true
+}
+
+
+export function commandStrike(editor: EditorView) {
+    editor.dispatch(editor.state.changeByRange((range: SelectionRange) => replaceInlineCommand(editor, range, '~~')))
+    return true
+}
+
+export function commandFormatMarkdown(editor: EditorView) {
+    prettier.format(getDocString(editor), { semi: false, parser: 'markdown', plugins: [pluginMarkdown] })
+        .then((formatContent: any) => {
+            let maxLen = getDocLength(editor)
+            let position = editor.state.selection.main.from
+            insert(editor, 0, maxLen, formatContent, position, position)
+        })
+    return true
+}
