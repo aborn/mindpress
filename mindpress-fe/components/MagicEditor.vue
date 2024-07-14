507b0e44f9b3f081ed0403ebf973e059ddd51ef0
aborn <loveaborn@foxmail.com>
2024-07-14 20:49:48 +0800

    mac hotkey adapt.

--- mindpress-fe/components/MagicEditor.vue
+++ mindpress-fe/components/MagicEditor.vue
@@ -57,7 +57,12 @@
                         </svg>
                     </span>
                     <span class="toolbaritem" @click="toobarItemAction('copyToWeChat')">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15.85 8.14c.39 0 .77.03 1.14.08C16.31 5.25 13.19 3 9.44 3c-4.25 0-7.7 2.88-7.7 6.43c0 2.05 1.15 3.86 2.94 5.04L3.67 16.5l2.76-1.19c.59.21 1.21.38 1.87.47c-.09-.39-.14-.79-.14-1.21c-.01-3.54 3.44-6.43 7.69-6.43M12 5.89a.96.96 0 1 1 0 1.92a.96.96 0 0 1 0-1.92M6.87 7.82a.96.96 0 1 1 0-1.92a.96.96 0 0 1 0 1.92"/><path fill="currentColor" d="M22.26 14.57c0-2.84-2.87-5.14-6.41-5.14s-6.41 2.3-6.41 5.14s2.87 5.14 6.41 5.14c.58 0 1.14-.08 1.67-.2L20.98 21l-1.2-2.4c1.5-.94 2.48-2.38 2.48-4.03m-8.34-.32a.96.96 0 1 1 .96-.96c.01.53-.43.96-.96.96m3.85 0a.96.96 0 1 1 0-1.92a.96.96 0 0 1 0 1.92"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="24" viewBox="0 0 24 24">
+                            <path fill="currentColor"
+                                d="M15.85 8.14c.39 0 .77.03 1.14.08C16.31 5.25 13.19 3 9.44 3c-4.25 0-7.7 2.88-7.7 6.43c0 2.05 1.15 3.86 2.94 5.04L3.67 16.5l2.76-1.19c.59.21 1.21.38 1.87.47c-.09-.39-.14-.79-.14-1.21c-.01-3.54 3.44-6.43 7.69-6.43M12 5.89a.96.96 0 1 1 0 1.92a.96.96 0 0 1 0-1.92M6.87 7.82a.96.96 0 1 1 0-1.92a.96.96 0 0 1 0 1.92" />
+                            <path fill="currentColor"
+                                d="M22.26 14.57c0-2.84-2.87-5.14-6.41-5.14s-6.41 2.3-6.41 5.14s2.87 5.14 6.41 5.14c.58 0 1.14-.08 1.67-.2L20.98 21l-1.2-2.4c1.5-.94 2.48-2.38 2.48-4.03m-8.34-.32a.96.96 0 1 1 .96-.96c.01.53-.43.96-.96.96m3.85 0a.96.96 0 1 1 0-1.92a.96.96 0 0 1 0 1.92" />
+                        </svg>
                     </span>
                     <span class="toolbaritem" @click="toobarItemAction('pagefull')">
                         <svg v-if="!fullPage" xmlns="http://www.w3.org/2000/svg" width="28" height="24"
@@ -116,11 +121,10 @@ import { languages } from "~/unjs/editor/codemirror/language-data/language-data"
 import { HighlightStyle, syntaxHighlighting } from "@codemirror/language"
 // @ts-ignore
 import { ViewUpdate, keymap, BlockInfo } from '@codemirror/view'
-import { EditorState } from '@codemirror/state'
 import { basicLight, basicLightTheme, basicLightHighlightStyle } from "~/unjs/editor/themes/default-theme"
 import { oneDark } from '@codemirror/theme-one-dark'
-import { EditorSelection, SelectionRange, Compartment } from '@codemirror/state'
-import { wxRenderer } from "~/unjs/render/wxRenderer";
+import { EditorSelection, SelectionRange, Compartment, EditorState } from '@codemirror/state'
+import { wxRenderer } from "~/unjs/render/wxRenderer"
 import { forceToArray, isBlank } from "~/unjs/utils"
 import { copyToWechat, mergeCss, solveWeChatImage } from "~/unjs/editor/wechat"
 const debounce = createDebounce()
@@ -369,11 +373,10 @@ export default {
     mounted: function () {
         this.createArea(this.content);
         document.addEventListener('keydown', e => {
-            if (e.ctrlKey && e.key === 's') {
+            // windows: Ctrl + S, mac: Command + s
+            if ((e.ctrlKey || e.metaKey) && e.key === 's') {
                 // Prevent the Save dialog to open
                 e.preventDefault();
-                // Place your code here
-                console.log('CTRL + S');
                 if (!this.editor) {
                     console.error('editor instance doesnot exists!')
                 }
@@ -391,7 +394,6 @@ export default {
          * document.addEventListener('mozfullscreenchange', exitHandler);
          * document.addEventListener('MSFullscreenChange', exitHandler);
          * */
-
         document.addEventListener('fullscreenchange', e => {
             if (document.fullscreenElement) {
                 console.log('enter fullscreen')
