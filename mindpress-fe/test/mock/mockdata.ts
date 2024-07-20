3b8b33130cc5512fb52787a9a31e786c6b1aacf1
aborn <loveaborn@foxmail.com>
2024-07-20 23:04:36 +0800

    add mock data

---
+++ mindpress-fe/test/mock/mockdata.ts
@@ -0,0 +1,82 @@
+export const MOCK_MD_CONTENT = `
+---
+title: 'Getting Started'
+date: '2024-07-18 11:36:55'
+authors: [{"name":"Aborn Jiang","link":"aborn.jiang"},{"name":"John","link":"aborn.jiang"}]
+permalink: '/article/8dd81e'
+category: 
+  - MindPress
+  - Guide
+tag: 
+  - Guide
+  - API
+createTime: '2024-06-05 22:29:37'
+mpid: '8dd81e'
+mpstatus: 'draft'
+---
+
+<!-- Content of the page -->
+
+
+# MindPress
+Press your mind gracefully, which is powered by nuxt.js 3.0 and nuxt.js content 2.0. You can use it for your personal site.
+
+## Setup
+
+Make sure to install the dependencies:
+
+\`\`\`bash
+# yarn
+yarn install
+
+# npm
+npm install
+
+# pnpm
+pnpm install --shamefully-hoist
+\`\`\`
+
+## Development Server
+
+Start the development server on http://localhost:3000
+
+\`\`\`bash
+npm run dev
+\`\`\`
+
+## Production
+
+Build the application for production:
+
+\`\`\`bash
+npm run build
+\`\`\`
+
+Locally preview production build:
+
+\`\`\`bash
+npm run preview
+\`\`\`
+
+Checkout the [deployment documentation](https://v3.nuxtjs.org/docs/deployment) for more information.
+`
+
+export const MOCK_MD_HEADER = `
+---
+title: 'Getting Started'
+date: '2024-07-18 11:36:55'
+authors: [{"name":"Aborn Jiang","link":"aborn.jiang"},{"name":"John","link":"aborn.jiang"}]
+permalink: '/article/8dd81e'
+category: 
+  - MindPress
+  - Guide
+tag: 
+  - Guide
+  - API
+createTime: '2024-06-05 22:29:37'
+mpid: '8dd81e'
+mpstatus: 'publish'
+---
+
+<!-- Content of the page -->
+`
