7cf9a768059630325f10a14f3ed58df0407a1247
aborn <loveaborn@foxmail.com>
2024-07-23 11:44:20 +0800

    ToolBarIcon & ToolBarDivider & toolbar refact.

---
+++ mindpress-fe/components/Editor/ToolBarDivider.vue
@@ -0,0 +1,9 @@
+<template>
+    <div style="display: inline-flex;">
+        <span style="display: inline-flex;align-items: center;padding: 4px 4px;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16">
+                <path fill="currentColor" d="M7.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-1 0v-9a.5.5 0 0 1 .5-.5" />
+            </svg>
+        </span>
+    </div>
+</template>
